0b53c62be00043e692d5c35048a1d057c81f1bf5
Toby <toby@vsus.app>
2023-10-04 20:39:19 -0700

    fix: :ambulance:

--- commands/delete.ts
+++ commands/delete.ts
@@ -58,7 +58,7 @@ export default new Command({
     events.get(interaction.user.id)!.get(interaction.options.getString("event")!)!.start();
 
     interaction.reply({
-      content: "Started event",
+      content: "Deleted event",
       ephemeral: true,
     });
   },
